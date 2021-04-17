e6384c96fad2b6e30b91dec0369730d9b5c94191
nelinory <15143882+nelinory@users.noreply.github.com>
2021-04-17 16:32:12 -0500

    Styled the scrollbar for notes view. Added attached property to allow proper handling of PreviewMouseWheel event.

---
+++ Tefterly.Core/Resources/Controls/ScrollViewerCorrector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Tefterly.Core.Resources.Controls
+{
+    // Credit: https://serialseb.com/blog/2007/09/03/wpf-tips-6-preventing-scrollviewer-from
+    public class ScrollViewerCorrector
+    {
+        public static bool GetFixScrolling(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(FixScrollingProperty);
+        }
+
+        public static void SetFixScrolling(DependencyObject obj, bool value)
+        {
+            obj.SetValue(FixScrollingProperty, value);
+        }
+
+        public static readonly DependencyProperty FixScrollingProperty =
+            DependencyProperty.RegisterAttached("FixScrolling", typeof(bool), typeof(ScrollViewerCorrector), new FrameworkPropertyMetadata(false, OnFixScrollingPropertyChanged));
+
+        public static void OnFixScrollingPropertyChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ScrollViewer viewer = sender as ScrollViewer;
+
+            if (viewer == null)
+                throw new ArgumentException("The dependency property can only be attached to a ScrollViewer", "sender");
+
+            if ((bool)e.NewValue == true)
+                viewer.PreviewMouseWheel += HandlePreviewMouseWheel;
+            else if ((bool)e.NewValue == false)
+                viewer.PreviewMouseWheel -= HandlePreviewMouseWheel;
+        }
+
+        private static List<MouseWheelEventArgs> _reentrantList = new List<MouseWheelEventArgs>();
+
+        private static void HandlePreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollControl = sender as ScrollViewer;
+
+            if (e.Handled == false && sender != null && _reentrantList.Contains(e) == false)
+            {
+                var previewEventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+                {
+                    RoutedEvent = UIElement.PreviewMouseWheelEvent,
+                    Source = sender
+                };
+
+                var originalSource = e.OriginalSource as UIElement;
+                _reentrantList.Add(previewEventArg);
+                originalSource.RaiseEvent(previewEventArg);
+                _reentrantList.Remove(previewEventArg);
+
+                // at this point if no one else handled the event in our children, we do our job
+                if (previewEventArg.Handled == false && ((e.Delta > 0 && scrollControl.VerticalOffset == 0)
+                    || (e.Delta <= 0 && scrollControl.VerticalOffset >= scrollControl.ExtentHeight - scrollControl.ViewportHeight)))
+                {
+                    e.Handled = true;
+                    var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                    eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                    eventArg.Source = sender;
+                    var parent = (UIElement)((FrameworkElement)sender).Parent;
+                    parent.RaiseEvent(eventArg);
+                }
+            }
+        }
+    }
+}
