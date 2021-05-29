465a8812fb4f30ddc332b66edf81899528ab0752
nelinory <15143882+nelinory@users.noreply.github.com>
2021-05-29 18:33:00 -0500

    Disabled the system menu for the application

--- Tefterly/Views/MainWindow.xaml.cs
+++ Tefterly/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
 using Tefterly.Services;
 
 namespace Tefterly.Views
@@ -17,6 +19,7 @@ namespace Tefterly.Views
             InitializeComponent();
 
             Closing += OnClosing;
+            Loaded += OnLoaded;
 
             // attach all required services
             _noteService = noteService;
@@ -30,6 +33,26 @@ namespace Tefterly.Views
 
             // save settings before exiting
             _settingsService.Save();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr windowHandle = new WindowInteropHelper(this).Handle;
+
+            HwndSource handleSource = HwndSource.FromHwnd(windowHandle);
+            handleSource.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        private IntPtr WndProc(IntPtr handle, int message, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            uint WM_SYSCOMMAND = 0x112;
+            uint SC_KEYMENU = 0xf100;
+
+            // do not allow the system menu to show up
+            if ((message == WM_SYSCOMMAND) && (wParam.ToInt32() == SC_KEYMENU))
+                handled = true;
+
+            return IntPtr.Zero;
         }
     }
 }
